69951a1c9eae8892cc1f5e3230faf978fa271665
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-07-06 12:11:33 -0500

    updates (not tested)

--- README.md
+++ README.md
@@ -1,16 +1,53 @@
 # simple-sync-service
 
+We provide two simple folder watchers for flows.
+The example flows can be found in detail at globus-jupyter-notebooks](https://github.com/globus/globus-jupyter-notebooks).
 
 ## Install 
+This examples require only the `globus_automate_client` and `watchdog` packages. They can be installed by setting a new environment with:
 
      pip install -r requirements.txt
 
 ## Deploy flow
+Two flows are provided for this examples, the first executes a `folder transfer` and `set permissions` while the second also `publishes` metadata in a globus portal. 
 
-     ./simple_sync_def.py
+Both scripts can be executes to register a new flow with globus services:
+
+    ./def_simple_sync.py
+    ./def_simple_sync_publish.py
 
 ## Deploy Client
 
-`local_dir` defines the folder to watch.
+The clients (`simple_sync.py` and `simple_sync_publish.py` are executables with the following inputs:
 
-     ./simplesync.py $local_dir
+`--local_dir` defines the folder to watch.
+`--includes` defines the file types that will trigger the flow. This can receive a list separated by spaces.
+
+```bash
+./simplesync.py --local_dir $local_dir --include .txt
+```
+
+## Client logic
+
+The trigger logic can be easily modified on `folder_watch.py`
+
+```python
+class Handler(FileSystemEventHandler):
+def __init__(self, ClientLogic, include_filters):
+     super(FileSystemEventHandler).__init__()
+     self.logic_function = ClientLogic
+     self.include_filters = include_filters
+def on_any_event(self, event):
+     if event.event_type == 'created':
+          print("File created: "+ os.path.basename(event.src_path))
+          for pattern in self.include_filters:
+               if event.src_path.endswith(pattern):
+                    print("File with " + pattern)
+                    print("Starting Flow")
+                    self.logic_function(event.src_path)
+                    return None
+```
+In this case. The flow will be started everytime a file is created and endswith the filetype on `--includes`
+
+It is important to notice that the logic can also be included on the client file `simple_sync.py`.
+
--- simple_sync_publish.py
+++ simple_sync_publish.py
@@ -12,14 +12,14 @@ def run_sync_flow(event_file):
     
     # Using a flow that was deployed using the "Automation Using Globus Flows" notebook
     # To do: update to flow that needs to be run 
-    flow_id = '6d22887b-f192-4b22-9cdc-67cfa3405ae8'
+    flow_id = '9f330207-3db6-4f57-8c72-8253922ae754'
     # To do: update the scope for the flow
-    flow_scope = 'https://auth.globus.org/scopes/6d22887b-f192-4b22-9cdc-67cfa3405ae8/flow_6d22887b_f192_4b22_9cdc_67cfa3405ae8_user'
+    flow_scope = 'https://auth.globus.org/scopes/9f330207-3db6-4f57-8c72-8253922ae754/flow_9f330207_3db6_4f57_8c72_8253922ae754_user'
     
     # Source is the endpoint where this trigger code is running
     # To do: update to id of the endpoint where this code is running
     source_id = 'e7d4f216-9a9a-11ea-8ece-02c81b96a709'
-   
+    
     # To do: update to destination endpoint, 
     # Must be a shared endpoint or guest collection so permission can be set
     destination_id = 'b7641b2a-f74a-11ec-835d-cd84b862b754'
@@ -35,11 +35,29 @@ def run_sync_flow(event_file):
     # for moving a directory
     event_folder = os.path.dirname(event_file)
     source_path = os.path.join(event_folder, "") 
-
+    
     # to get a resonable label, using the file that triggered the run
     event_file_name = os.path.basename(event_file)
+   
+    # to get a resonable label, using the file that triggered the run
+    event_file_name = os.path.basename(event_file)
+    
+    # to be able to set permission on the specific folder being moved
+    # the name of the source folder needs to be worked out to use in 
+    # destination path
+    event_folder_name = os.path.basename(event_folder)
+    # Add a slash to meet Transfer requirements for directory transfer
+    destination_path = os.path.join(remote_path, event_folder_name, "")
 
+    #Globus search information
+    #To do: Specify the search index to publish your metadata 
+    search_index = '563c3d98-6fa8-4ef5-83e2-0f378efe0a5f'
 
+    #Gather some information for the transfer
+    #To do: add specific information and add it to "search_content_metadata" in the base_in
+    file_names = glob.glob(source_path+'*')
+    n_files = len(file_names)
+    
 
     # Base input for the flow
     base_input = {
@@ -54,16 +72,34 @@ def run_sync_flow(event_file):
                 "path": destination_path,
              },
             "recur": True,
+
             # Grant access to the Tutorial Users group
             "principal_identifier": group_id,
             "principal_type": "group",
 
+            #information to ingest on globus Search
+            "search_ingest_doc": {
+            "search_index": search_index,
+            "search_subject": event_folder_name,
+            "search_visible_to": ["public"],
+            "search_content_metadata": {
+                "title": event_folder_name,
+                "Username":"raf",
+                "fname": file_names,
+                "n_files":n_files
+            }
         }
+    }
     }
 
     run_result = fc.run_flow(flow_id = flow_id, flow_scope = flow_scope, flow_input= base_input, label=event_file_name, tags=['PEARC_Test'])
     print('Moving and sharing: ' + event_folder_name)
     print('https://app.globus.org/runs/'+run_result['run_id'])
+
+    print('Example portal for search index: ' + search_index)
+    print('https://acdc.alcf.anl.gov/globus-tutorial/' + search_index)
+    print('')
+    
 
 # Arg Parsing
 def parse_args():
